f2d1397df667cde546f0218fb02e75c26d00b60c
贾辰 <951311987@qq.com>
2018-08-09 23:31:14 +0800

    创建项目

---
+++ PaintHelper/PaintHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaintHelper
+//
+//  Created by 贾辰 on 18/8/9.
+//  Copyright © 2018年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PaintHelper/PaintHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PaintHelper
+//
+//  Created by 贾辰 on 18/8/9.
+//  Copyright © 2018年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
